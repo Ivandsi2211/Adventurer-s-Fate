ab7d8fba15daa2cd1bc01461f1a2a092eb2aeb50
Ivandsi <ivandsi2211@gmail.com>
2018-05-02 20:07:50 +0200

    Hora de guardar cambios
    Ahora tiene Sprites y algunos Scripts. He hecho un simple cambio de escenas en el proyecto.

---
+++ Assets/Scripts/NewGameButtonController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+public class NewGameButtonController : MonoBehaviour
+{
+    public GameObject upSprite;
+    public GameObject downSprite;
+    public float downTime = 0.1f;
+    private enum buttonStates
+    {
+        up = 0,
+        down
+    }
+    private buttonStates currentState = buttonStates.up;
+    private float nextStateTime = 0.0f;
+    void Start()
+    {
+        upSprite.SetActive(true);        downSprite.SetActive(false);
+    }
+    void OnMouseDown()
+    {
+        if (nextStateTime == 0.0f
+         &&
+         currentState == NewGameButtonController.buttonStates.up)
+        {
+            nextStateTime = Time.time + downTime;
+            upSprite.SetActive(false);
+            downSprite.SetActive(true);
+            currentState = NewGameButtonController.buttonStates.down;
+        }
+    }
+    void Update()
+    {
+        if (nextStateTime > 0.0f)
+        {
+            if (nextStateTime < Time.time)
+            {
+                // Retornar el botó a estat “no polsat”
+                nextStateTime = 0.0f;
+                upSprite.SetActive(true);
+                downSprite.SetActive(false);
+                currentState = NewGameButtonController.buttonStates.up;
+                // Començar el joc
+                Application.LoadLevel("Game");
+            }
+        }
+    }
+}
